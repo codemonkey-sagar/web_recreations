dbaca43a9dcdcab74628cbf86ad384fd023f1cdf
Sagar Budhathoki <fmtsagar@gmail.com>
2023-08-21 23:10:07 +0545

    Announcement Bar: Completed

--- react_navigation/script.js
+++ react_navigation/script.js
@@ -1,2 +1,23 @@
-// cache the user action on announcementBar 
- 
+// Cache the user action on announcementBar 
+const announcementBtn = document.querySelector(".announcementBar--close");
+
+// Get the value from Local Storage 
+let isDismissed = localStorage.getItem("announcement.dismiss") === "true";
+
+// Hide the announcement if dismissed
+if (isDismissed) {
+  announcementBtn.parentElement.classList.add("hidden");
+}
+
+// Add click event listener to announcementBtn 
+announcementBtn.addEventListener("click", function () {
+  // If it's already dismissed, no need to do anything 
+  if (isDismissed) {
+    return;
+  }
+
+  // Update value and hide announcement 
+  localStorage.setItem("announcement.dismiss", "true");
+  announcementBtn.parentElement.classList.add("hidden");
+  isDismissed = true;
+})
